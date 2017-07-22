9a2c85cb3ccfeeca1ced9f1e46d4357ca5736027
devjob2012 <devjob.2012@gmail.com>
2017-07-22 09:03:29 +0100

    Initial version

---
+++ Java8Optional/src/main/java/co/uk/dev/java8/examples/ExampleOptional.java
@@ -0,0 +1,13 @@
+package co.uk.dev.java8.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class ExampleOptional 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
