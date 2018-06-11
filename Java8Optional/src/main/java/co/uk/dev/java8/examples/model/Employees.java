fecb07d3583cc9a374b0dc7f9c7c68b7325c637f
Deven Rawat <devenrawat@metrobank.plc.uk>
2018-06-11 12:49:23 +0100

    Added new file

---
+++ Java8Optional/src/main/java/co/uk/dev/java8/examples/model/Employees.java
@@ -0,0 +1,12 @@
+package co.uk.dev.java8.examples.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Employees {
+	private String name;
+	private String dept;
+	private int salary;
+}
