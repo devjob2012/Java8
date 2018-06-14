08bb22fcc41a4621b901dfc453865f2bd2ac271b
Deven Rawat <devenrawat@metrobank.plc.uk>
2018-06-14 13:23:42 +0100

    change

--- Java8Streams/src/main/java/co/uk/deven/java8/example/JavaStreamExamples.java
+++ Java8Streams/src/main/java/co/uk/deven/java8/example/JavaStreamExamples.java
@@ -1,7 +1,24 @@
 package co.uk.deven.java8.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JavaStreamExamples {
 	public static void main(String[] args) {
+		JavaStreamExamples example = new JavaStreamExamples();
+		example.variousSamples();
+
+		Map<String, String> map = new HashMap<>();
+		map.put("C", "c");
+		map.put("B", "b");
+		map.merge("B", "NEW", (v1, v2) -> v1 + v2);
+		System.out.println(map.get("B")); // prints bNEW
+
+		map.merge("B", "-->HELLO", (t, u) -> t + u);
+		System.out.println(map.get("B"));
+	}
+
+	public void variousSamples() {
 
 	}
 }
