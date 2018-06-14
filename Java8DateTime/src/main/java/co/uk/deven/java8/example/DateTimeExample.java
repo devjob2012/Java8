2b557c6f6a00446922146b77d9923bb023229ec4
Deven Rawat <devenrawat@metrobank.plc.uk>
2018-06-14 15:16:36 +0100

    initial version

--- Java8DateTime/src/main/java/co/uk/deven/java8/example/DateTimeExample.java
+++ Java8DateTime/src/main/java/co/uk/deven/java8/example/DateTimeExample.java
@@ -1,16 +1,25 @@
 package co.uk.deven.java8.example;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Local Date
  *
  */
 public class DateTimeExample {
-	public static void main( String[] args )
-    {
-    	LocalDate.of(2016, 1, 31).plusMonths(1);
+	public static void main(String[] args) {
+		LocalDate.of(2016, 1, 31).plusMonths(1);
 
-        System.out.println( "Hello World!" );
-    }
+		LocalDate dtNow = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("yyyy/dd/MM");
+		LocalDate dt1 = LocalDate.parse("12/23/2018", formatter);
+		System.out.println("Hello World!    " + dt1.format(newFormatter));
+		System.out.println("" + dt1.until(dtNow));
+		LocalTime time = LocalTime.now();
+		System.out.println(" Local Time  " + time);
+
+	}
 }
